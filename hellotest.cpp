60c5a8771bbd2ce20383a3a9096943f0768db814
santia6 <santia6@stolaf.edu>
2025-05-27 15:16:14 -0500

    GitHub connection test

---
+++ hellotest.cpp
@@ -0,0 +1,9 @@
+// Simple test file testing connection with GitHub
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl; //print hello world
+    return 0;
+}
